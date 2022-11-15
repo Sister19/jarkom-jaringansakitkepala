05700f61cf4c616e6ed27352f5422e209342fa0a
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-11-16 06:27:16 +0700

    fix(conn): nambah return addr di listen segment

--- lib/connection.py
+++ lib/connection.py
@@ -15,12 +15,12 @@ class Connection:
         # Send single segment into destination
         self.sock.sendto(msg.get_bytes(), dest)
 
-    def listen_single_segment(self) -> Segment:
+    def listen_single_segment(self) -> Tuple[Segment, Tuple[str, int]]:
         # Listen single UDP datagram within timeout and convert into segment
         data, addr = self.sock.recvfrom(LISTEN_BUFFER_SIZE)
         msg = Segment()
         msg.set_from_bytes(data)
-        return msg
+        return msg, addr
 
     def set_timeout(self, timeout: float):
         self.sock.settimeout(timeout)
